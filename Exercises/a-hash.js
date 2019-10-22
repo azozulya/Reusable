a20e401a95520df6e4da51dbfc8ebc9ac6564c83
azozulya <bbbom@mail.ru>
2019-10-22 15:03:30 +0300

    Lab a-hash some fix

--- Exercises/a-hash.js
+++ Exercises/a-hash.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const phonebook = {
-  Anna: { phone: '+79923523555' },
-  Michail: { phone: '+79086788333' },
-  Margo: { phone: '+79267885444' },
-  Alex: { phone: '+79052352222' },
+  Anna: '+79923523555',
+  Michail: '+79086788333',
+  Margo: '+79267885444',
+  Alex: '+79052352222',
 };
 
-const findPhoneByName = name => phonebook[name].phone;
+const findPhoneByName = name => phonebook[name];
 
 module.exports = { phonebook, findPhoneByName };
